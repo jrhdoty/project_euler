ac7e60ba665867fc985068ecf510d0fe8539a6bd
John Doty <jrhdoty@gmail.com>
2014-08-02 16:58:17 -0700

    create helper functions for prime checking and memoize

---
+++ project_euler_javascript/problem_27.js
@@ -0,0 +1,69 @@
+/*
+Euler discovered the remarkable quadratic formula:
+
+n² + n + 41
+
+It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. 
+However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, 
+and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+
+The incredible formula  n² − 79n + 1601 was discovered, 
+which produces 80 primes for the consecutive values n = 0 to 79. 
+The product of the coefficients, −79 and 1601, is −126479.
+
+Considering quadratics of the form:
+
+n² + an + b, where |a| < 1000 and |b| < 1000
+
+where |n| is the modulus/absolute value of n
+e.g. |11| = 11 and |−4| = 4
+Find the product of the coefficients, a and b, 
+for the quadratic expression that produces the maximum number of primes for consecutive values of n, 
+starting with n = 0.
+*/
+
+var isPrime = function(num){
+  if (num < 2){
+    return false;
+  }
+  if (num === 2){
+    return true;
+  }
+  if ( num % 2 === 0){return false;}
+
+  var root = Math.sqrt(num);
+  for ( var i = 3; i < root; i+=2 ){
+    if ( num % i === 0){
+      return false;
+    }
+  }
+  return true;
+};
+
+var memoize = function(func){
+  var results = {};
+  return function(item){
+    if (results[item] === undefined){results[item] = func(item);}
+    return results[item];
+  };
+};
+
+var checkQuadraticPrimeGenerator = function(a, b){
+  //return number of primes generated 0-n
+  var prime = true;
+  var n = 0;
+  while(prime){
+    
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
