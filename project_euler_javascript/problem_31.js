15c953793dcf1845f3b1dedc2bc6086957f57260
John Doty <jrhdoty@gmail.com>
2014-08-05 16:51:30 -0700

    add solution to problem 31"

--- project_euler_javascript/problem_31.js
+++ project_euler_javascript/problem_31.js
@@ -1,3 +1,4 @@
+/*
 Coin sums
 Problem 31
 Published on 22 November 2002 at 06:00 pm [Server Time]
@@ -8,3 +9,24 @@ It is possible to make £2 in the following way:
 
 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 How many different ways can £2 be made using any number of coins?
+*/
+
+var coinSums = function(target, options){
+  return  coinSumsRec(target, options, 0);
+};
+
+var coinSumsRec = function(target, options, index){
+  if (target === 0){
+    return 1;
+  }
+  if( index >= options.length || target < 0){
+    return 0;
+  }
+  var result = 0;
+  result += coinSumsRec(target - options[index], options, index);
+  result += coinSumsRec(target, options, index+1);
+
+  return result;
+};
+
+console.log(coinSums(200, [1, 2, 5, 10, 20, 50, 100, 200]));
