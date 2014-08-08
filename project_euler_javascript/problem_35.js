5d16a58cc5ee04c8096fbe647e42ded37182f1de
John Doty <jrhdoty@gmail.com>
2014-08-07 17:59:11 -0700

    setup file for problem 35

---
+++ project_euler_javascript/problem_35.js
@@ -0,0 +1,14 @@
+/*
+Circular primes
+Problem 35
+Published on 17 January 2003 at 06:00 pm [Server Time]
+The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+
+How many circular primes are there below one million?
+*/
+
+var rotations = function(num){
+
+};
