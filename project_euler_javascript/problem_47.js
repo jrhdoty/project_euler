3ed871863c85d0ccd5a16e9aa716c17541faa8ef
John Doty <jrhdoty@gmail.com>
2014-08-13 15:05:19 -0700

    add prime factoring function to problem 47

---
+++ project_euler_javascript/problem_47.js
@@ -0,0 +1,31 @@
+/*
+Distinct primes factors
+Problem 47
+Published on 04 July 2003 at 06:00 pm [Server Time]
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 × 7
+15 = 3 × 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2² × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
+
+Find the first four consecutive integers to have four distinct prime factors. What is the first of these numbers?
+*/
+
+var primeFactors = function(num){
+  var factors = {};
+  var current = 2;
+  while (num > 1){
+    if (num % current === 0){
+      factors[current] ? factors[current] += 1 : factors[current] = 1;
+      num/=current;
+    } else{
+      current += 1;
+    }
+  }
+  return factors;
+};
