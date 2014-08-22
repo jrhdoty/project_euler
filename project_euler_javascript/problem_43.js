034b9b667153355fe7f4eeaf148a1bf1aac4bce2
John Doty <jrhdoty@gmail.com>
2014-08-22 10:44:16 -0700

    add function for generating all pandigitals

---
+++ project_euler_javascript/problem_43.js
@@ -0,0 +1,70 @@
+/*Problem 43
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+d2d3d4=406 is divisible by 2
+d3d4d5=063 is divisible by 3
+d4d5d6=635 is divisible by 5
+d5d6d7=357 is divisible by 7
+d6d7d8=572 is divisible by 11
+d7d8d9=728 is divisible by 13
+d8d9d10=289 is divisible by 17
+Find the sum of all 0 to 9 pandigital numbers with this property.
+*/
+
+var range = function(min, max){
+  var result = [];
+  for ( var i = min; i < max; i++){
+    result.push(i);
+  }
+  return result;
+};
+
+var pandigitals = function(max){
+  var results = [];
+  var choices = range(0, max+1);
+  var i;
+  for ( i = 0; i < choices.length; i++){
+    choices[i] = choices[i].toString();
+  }
+  var remaining, next;
+
+  var pandigitalsRec = function(current, options){
+    if (options.length === 0){
+      results.push(current);
+      return;
+    }
+    var next, remaining;
+    for ( var i = 0; i < options.length; i++ ){
+      remaining = options.slice(0);
+      next = remaining.splice(i, 1);
+      pandigitalsRec(current+next, remaining);
+    }
+  };
+
+  for ( i = 1; i < choices.length; i++){
+    remaining = choices.slice(0);
+    next = remaining.splice(i, 1);
+    pandigitalsRec(next, remaining);
+  }
+
+  return results;
+};
+
+
+// var specialProperty = function(str){
+//   var length = str.length - 2;
+//   for ( var i = 2; i < length; i++ ){
+    
+//   }
+// };
+
+
+// d2d3d4=406 is divisible by 2
+// d3d4d5=063 is divisible by 3
+// d4d5d6=635 is divisible by 5
+// d5d6d7=357 is divisible by 7
+// d6d7d8=572 is divisible by 11
+// d7d8d9=728 is divisible by 13
+// d8d9d10=289 is divisible by 17
